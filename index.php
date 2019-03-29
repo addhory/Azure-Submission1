b620821639d89f5b6542975c058510d129de1cb0
xxridhohero11 <muhridhoanshory@gmail.com>
2019-03-29 23:51:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
             if(count($registrants) > 0) {
                 echo "<h2>People who are registered:</h2>";
                 echo"<div>";
-                echo "<table border='8'>";
+                echo "<table border=''>";
                 echo "<tr><th>Name</th>";
                 echo "<th>  <th>";
                 echo "<th>Email</th>";
